45c856d7500d75c58d14cb63328580fff820ff6a
Shahadat Khan <110185257+Shahadattkhann@users.noreply.github.com>
2023-02-24 11:28:14 +0530

    valid print statement is added

--- emailvalidation.py
+++ emailvalidation.py
@@ -1,5 +1,5 @@
 email = input("Please Enter Your Email: ")
-d = 0
+x,y,z = 0,0,0
 if len(email)>6:
     if email[0].isalpha():
          if "@" in email and email.count("@")==1:
@@ -12,18 +12,24 @@ if len(email)>6:
                              continue
                          elif i.isalpha():
                              if i==i.upper():
-                                print("Invalid Email, Email must not contain any upper letter")
+                                print("Invalid Email, Email must not contain any upper letter, please remove upper letters from your Email")
+                                x = 1
                          elif i==" ":
-                             print("Invalid email, Email must not contain any spaces")
+                             print("Invalid email, Email must not contain any spaces, please remove all the spaces in your Email")
+                             y = 1
                          else:
-                             print("Invalid Email, Email must not contain any special character")    
-                     print(f"{email} is Valid")              
+                             print("Invalid Email, Email must not contain any special character")
+                             z = 1 
+                     if x==1 or y==1 or z==1:
+                         print("Please correct your email and try again ")
+                     else:
+                         print("Valid email")              
              else:
-                 print("Invalid Email, Email must contain . from last 3 or 4 index")
+                 print("Invalid Email, Email must contain . from last 3 or 4 index, ex: .com, .in")
          else:
-             print("Invalid Email, Email must contain '@' only once.")
+             print("Invalid Email, Email must contain '@' only once. remove all extra @ from Your Email")
     else:
         print("Invalid Email, Email must contain 1st letter as an Alphabet")
 else:
-    print("Invalid Email, Email Length is too Short")
+    print("Invalid Email, Email Length is too Short, lenth must be greter than 6")
 
